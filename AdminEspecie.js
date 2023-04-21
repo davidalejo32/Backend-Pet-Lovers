2dc168d8dae35550a3f4dfb1562da4b7c85fe04d
David Claros <davidalejo32@hotmail.com>
2023-04-20 19:30:13 -0500

    add: clase adminEspecie crea especies y lista especies

---
+++ AdminEspecie.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+
+class AdminEspecie {
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async crearEspecie(req, res) {
+    const datos = req.body;
+
+    const especie = await this.prisma.especie.create({
+      data: datos,
+    });
+
+    res.json(especie);
+  }
+
+  async listarEspecies(req, res) {
+    const especies = await this.prisma.especie.findMany();
+
+    res.json(especies);
+  }
+}
+
+module.exports = AdminEspecie;
